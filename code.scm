259e36ef8a1e612f79259acc8d94d866e63b094f
darthspectrum <muradianator@gmail.com>
2023-08-31 01:33:56 +0200

    chore: halfway through cons the magnificent

--- code.scm
+++ code.scm
@@ -30,3 +30,47 @@
 
 ; The First Commandment (preliminary):
 ;   Always ask `null?` as the first questionm in expressing any function,
+
+;;;;;;; Cons the Magnificent
+
+(define rember
+  (lambda (a lat)
+    (cond
+     ((null? lat) ())
+     ((eq? a (car lat)) (cdr lat))
+     (else (cons (car lat) (rember a (cdr lat)))))))
+
+(rember 'a '(a b c))	    ; (b c)
+(rember 'a '(a b a c))    ; (b a c)
+(rember 'a '(b a g a c))	; (b g a c)
+(rember 'a '(f g))		    ; (f g)
+(rember 'a ())			      ; ()
+
+; The Second Commandment
+;   Use `cons` to build lists
+
+
+(define firsts
+  (lambda (l)
+    (cond
+     ((null? l) ())
+     (else (cons (car (car l)) (firsts (cdr l)))))))
+
+(firsts ())                       ; ()
+(firsts '((a b) (c d) (e f)))     ; (a c e)
+(firsts '(((a b) c) (d e) (f)))   ; ((a b) d f)
+
+(define seconds
+  (lambda (l)
+    (cond
+     ((null? l) ())
+     (else (cons (car (cdr (car l))) (seconds (cdr l)))))))
+
+(seconds ())                              ; ()
+(seconds '((a b) (c d) (e f)))            ; (b d f)
+(seconds '(((a b) c) (d e) (f (g h))))    ; (c e (g h)))
+
+
+; The Third Commandment
+;   When building a list, describe the first typical element,
+;   and then `cons` it onto the natural recursion
